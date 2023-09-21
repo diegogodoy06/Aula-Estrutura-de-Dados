fb2cc1de6fd2779f633b6594c917fcd7bb53a75c
Diego Godoy <diegoalex-gdy@outlook.com>
2023-09-20 21:37:18 -0300

    Create Matriz Esparsa Aula 2.cpp

---
+++ Matriz Esparsa Aula 2.cpp
@@ -0,0 +1,29 @@
+void verificaOcupado(MatEsp * vetLin[], int Lin, int Col, MatEsp ** ret){
+	
+	MatEsp * aux = vetLin[Lin];
+	while(aux != NULL && Col > aux -> col){
+		aux = aux -> pl;
+	}
+	if(aux != NULL && Col == aux-> col)
+		*ret = aux;
+	else	
+		*ret = NULL;
+}
+
+void soma(MatEsp *vetLinA[], MatEsp vetLinB[], MatEsp *vetLinC[], MatEsp * vetColC[]){
+	MatEsp * aux;
+	int i,j,soma;
+	
+	for(i=0; i<nl; i++)
+		for(j=0; j<nc; j++){
+			soma = 0;
+			verificaOcupado(vetLinA, i j, &aux);
+			if(aux != NULL)
+				soma = aux -> valor;
+			verificaOcupado(vetLinB, i,j,&aux);
+			if(aux != NULL)
+				soma = soma +aux -> valor;
+			if(soma != 0)
+				insere(vetLinC, vetor i,j,soma);
+		}
+}
